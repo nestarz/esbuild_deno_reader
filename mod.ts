6923b05fdb46c57a785bebcd6e5ef67bf1241083
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-03-05 23:01:19 +0100

    chore: onResolve

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
-export default () => ({
+export default (onResolve) => ({
   name: "deno_read",
   setup(build) {
+    if (onResolve) build.onResolve({ filter: /.*\.(t|j)s(x|)/ }, onResolve);
     build.onLoad(
       { filter: /.*\.(t|j)s(x|)/, namespace: "file" },
       async ({ path }) => ({
